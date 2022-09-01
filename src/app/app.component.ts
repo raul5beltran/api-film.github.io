0279695341a6cbce4b96931d4ffc2e3701ac05fd
raul5beltran <raulbeltrandolz.rbd@gmail.com>
2022-09-01 19:32:51 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
-
+//app.component.ts
+import {Component, OnInit} from '@angular/core'; // Importing OnInit hook
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
+import { ThisReceiver } from '@angular/compiler';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api-angular';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  value:any;
+
+  getFilm(){
+    let aa=String(this.value);
+    console.log("Film entered is -- " + aa);
+    this.buscarpelicula(aa);
+    return aa;
+  }
+	title ='api-angular';
+  pel=this.getFilm();
+
+  buscarpelicula(_pelicula: string){
+    let peli=_pelicula
+    if(peli=='undefined'){
+      peli=''
+    }
+		const _url='https://imdb8.p.rapidapi.com/auto-complete?q='
+    fetch(_url+peli,{
+        "method": "GET",
+        "headers": {
+          "X-RapidAPI-Key": "2eb5faaa98msha54e6e251548835p153a74jsnc4e69658bf4c",
+          "X-RapidAPI-Host": "imdb8.p.rapidapi.com"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const list = data.d;
+        list.map((item: any)=>{
+          console.log(item)
+          const name=item.l;
+          const poster=item.i.imageUrl;
+          const año=item.y;
+          const rank=item.rank;
+          const type=item.qid;
+          const prota=item.s;
+          const movie=`
+          <li>
+          <img src="${poster}">
+          <h2>${name}</h2>
+          <div class="infopeli">
+            <p>Year ${año}</p>
+            <p>Ranking ${rank}</p>
+            <p>Protagonistas ${prota}</p>
+            <p>Tipo ${type}</p>
+          </div>
+          </li>`;
+          console.log(movie)
+          document.querySelector('.movies')!.innerHTML+=movie;
+        })
+      })
+      .catch(err => console.error(err));
+    }
+
+    ngOnInit() {
+	}
+
+
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'; // importing the http module
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { FormsModule } from '@angular/forms';
+//Rutas
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent,],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+    FormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
